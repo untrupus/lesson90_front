5c5d1482d3ecc6ae61fc9698f9d9f1e9e2a530ae
Ivan Volodartsev <untrupus@gmail.com>
2020-11-29 20:10:21 +0600

    create app, add canvas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useRef, useEffect} from 'react';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [state, setState] = useState({
+        mouseDown: false,
+        pixelsArray: []
+    });
+    const [options, setOptions] = useState({
+       color: '',
+       size: ''
+    });
+    const ws = useRef(null);
+
+    useEffect(() => {
+        ws.current = new WebSocket("ws://localhost:8000");
+
+        ws.current.onopen = () => {
+            ws.current.send(JSON.stringify({type: "GET_ALL_PIXELS"}));
+        };
+
+        ws.current.onclose = () => console.log("ws connection closed");
+        ws.current.onmessage = e => {
+            const decodedMessage = JSON.parse(e.data);
+            if (decodedMessage.type === "NEW_PIXEL") {
+                setState(messages => [...state.pixelsArray, decodedMessage.pixel]);
+            } else if (decodedMessage.type === "ALL_PIXELS") {
+                setState(messages => [...state.pixelsArray, ...decodedMessage.pixels]);
+            }
+        };
+
+        return () => ws.current.close();
+    }, []);
+
+    const canvas = useRef(null);
+
+    const canvasMouseMoveHandler = event => {
+        if (state.mouseDown) {
+            event.persist();
+            const clientX = event.clientX;
+            const clientY = event.clientY;
+            setState(prevState => {
+                return {
+                    ...prevState,
+                    pixelsArray: [...prevState.pixelsArray, {
+                        x: clientX,
+                        y: clientY,
+                    }]
+                };
+            });
+            const context = canvas.current.getContext('2d');
+            // context.fillStyle = "red;
+            // context.beginPath();
+            // context.arc(clientX, clientY, 12, 0, Math.PI * 2, true);
+            // context.closePath();
+            // context.fill();
+            const imageData = context.createImageData(1, 1);
+            const d = imageData.data;
+            d[0] = 0;
+            d[1] = 0;
+            d[2] = 0;
+            d[3] = 255;
+            context.putImageData(imageData, event.clientX, event.clientY);
+            console.log(state.pixelsArray);
+        }
+    };
+
+    const mouseDownHandler = event => {
+        setState({...state, mouseDown: true});
+    };
+
+    const mouseUpHandler = event => {
+        ws.current.send(JSON.stringify({
+            type: "CREATE_PIXEL",
+            pixels: state.pixelsArray
+        }));
+        setState({...state, mouseDown: false, pixelsArray: []});
+    };
+
+    const inputChangeHandler = e => {
+      const name = e.target.name;
+      const value = e.target.value;
+        setOptions(prevState => {
+            return {...prevState, [name]: value};
+        });
+    };
+
+    return (
+        <>
+            <div>
+                <canvas
+                    ref={canvas}
+                    style={{border: '1px solid black'}}
+                    width={800}
+                    height={600}
+                    onMouseDown={mouseDownHandler}
+                    onMouseUp={mouseUpHandler}
+                    onMouseMove={canvasMouseMoveHandler}
+                />
+            </div>
+            <input
+                type="color"
+                className="field"
+                name="color"
+                onClick={inputChangeHandler}
+            />
+            <input
+                type="text"
+                className="field"
+                placeholder="Size"
+                name="size"
+                onClick={inputChangeHandler}
+            />
+        </>
+    );
+
+};
 
 export default App;
+
